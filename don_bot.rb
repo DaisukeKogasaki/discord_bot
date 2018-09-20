5db9dd8293befcc65c6445dd141b5c2284f6afda
koga <potaton.fe31@gmail.com>
2018-09-20 18:59:19 +0900

    コードのりファクタ、RIOT APIでリーグデータを出すデモ、Embedのデモ作成

--- don_bot.rb
+++ don_bot.rb
@@ -51,16 +51,16 @@ bot.command(:goto, attributes = {:description => "don-bot will decide the destin
   end
 end
 
-url = 'https://status.discordapp.com/'
-charset = nil
-html = open(url) do |f|
-    charset = f.charset
-    f.read
-end
-
-doc = Nokogiri::HTML.parse(html, nil, charset)
-
 bot.command(:server_status, attributes = {:description => "Shows all server status at discord's voice chat."}) do |event|
+  url = 'https://status.discordapp.com/'
+  charset = nil
+  html = open(url) do |f|
+      charset = f.charset
+      f.read
+  end
+
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+
   doc.xpath('//div[@class="child-components-container "]').each do |node|
     server = []
     status = []
@@ -99,6 +99,37 @@ bot.command(:profile, attributes = {:description => "don-bot teaches your profil
   # message << "Role : #{event.user.roles}\n"
   message << "Avatar Image : #{event.user.avatar_url}\n"
   event.send_message message
+end
+
+riot_api_key = config["RiotAPI"]["KEY"]
+bot.command(:lol, attributes = { :description => "Show LoL's summoner profiles" }) do |event, sname, reg|
+  region = reg.nil? ? "jp1" : reg
+  summoner_name = sname
+  summoner_uri = URI.parse("https://#{region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/#{sname}?api_key=#{riot_api_key}")
+  return_summoner_data = Net::HTTP.get(summoner_uri)
+  summoer_info = JSON.parse(return_summoner_data)
+  return event.send_message "該当のサモナーが存在しません" unless summoer_info["status"].nil?
+  summoner_id = summoer_info["id"]
+  league_uri = URI.parse("https://#{region}.api.riotgames.com/lol/league/v3/positions/by-summoner/#{summoner_id}?api_key=#{riot_api_key}")
+  event.send_message "#{summoer_info["name"]}のリーグ情報"
+  return_league_data = Net::HTTP.get(league_uri)
+  league_info = JSON.parse(return_league_data)
+  return event.send_message "リーグ情報がありません" if league_info.empty?
+  message = "#{league_info[0]["leagueName"]}\n"
+  message << "#{league_info[0]["tier"]} #{league_info[0]["rank"]} (#{league_info[0]["leaguePoints"]}LP)\n"
+  win_late = (league_info[0]["wins"].to_f / (league_info[0]["wins"] + league_info[0]["losses"])).round(2) * 100
+  message << "#{league_info[0]["wins"]}wins/#{league_info[0]["losses"]}losses (#{win_late}%)"
+  event.send_message message
+end
+
+bot.command(:embed, attributes = {:description => "embed test."}) do |event|
+  event.channel.send_embed do |embed|
+    embed.title = 'fugafuga'
+    embed.url = 'https://qiita.com/tenmihi/items/23052f57256ccf3223d3'
+    embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: 'hoge-san')
+    embed.image = Discordrb::Webhooks::EmbedImage.new(url: 'https://i.imgur.com/PcMltU7.jpg')
+    embed.color = "#00ff00"
+  end
 end
 
 bot.run
